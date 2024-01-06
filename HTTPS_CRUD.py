afa3b7def705807df842f584aee65392ca20eedd
oliwier <12202053@student.pxl.be>
2024-01-07 00:23:06 +0100

    used runars HttpCRUD.py script

--- HTTPS_CRUD.py
+++ HTTPS_CRUD.py
@@ -1,55 +1,28 @@
-from datetime import datetime
 import requests
+from datetime import datetime
 
-#The CRUD-endpoint URL
-crud_endpoint_url = "iot.pxl.bjth.xyz/api/v1/temperature"
+# De URL van het CRUD-eindpunt
+crud_endpoint_url = "https://iot.pxl.bjth.xyz/api/v1/temperature"
 
+# Huidige timestamp verkregen in float value int(timestamp) voor omzetting naar int
+timestamp = int(datetime.now().timestamp())
+#data that will be put in the database
+payload = {
+	"id": timestamp, #retrieves time in seconds from 1970-now
+	"value": 0,	 #value is the temperature
+	"scale": "C"	 #type of data in this case Celcius
+}
 
-#Current time and date
-now = datetime.now()
-
-# Unix-tijd conversion
-timestamp = datetime.timestamp(now)
-
-#Headers
+#necessary info for the server
 headers = {
     "Content-Type": "application/json",
+    "X-Api-Key": ""#dont share this part!
 }
 
-# Read data from files
-with open('/sys/bus/iio/devices/iio:device0/in_temp0_raw', 'r') as f:
-    TEMP_RAW = int(f.read())
+#execute the crud "put" command 
+response =requests.put(crud_endpoint_url,headers=headers,json=payload)
 
-with open('in_temp0_scale', 'r') as f:
-    TEMP_SCALE = float(f.read())
-
-with open('in_temp0_offset', 'r') as f:
-    TEMP_OFFSET = float(f.read())
-
-#Calculate temperature
-TEMP = ((TEMP_RAW - TEMP_OFFSET) / TEMP_SCALE)
-
-#Format to send
-payload = {
-
- "id": timestamp,
-
- "value": TEMP,
-
- "scale": "Celsius"
-}
+# Print the response of the server in cmd
+print("Raw Response Content:", response.text)
 
 
-#Debugging code
-try:
-    #Send POST request
-    response = requests.post(crud_endpoint_url, json=payload, headers=headers)
-
-    # Controleer of het verzoek succesvol was (status code 2xx)
-    if response.status_code // 100 == 2:
-        print("Succesvol verstuurd naar het CRUD-eindpunt.")
-    else:
-        print(f"Fout bij het versturen naar het CRUD-eindpunt. Status code: {response.status_code}")
-
-except Exception as e:
-    print(f"Fout bij het uitvoeren van het verzoek: {str(e)}")
