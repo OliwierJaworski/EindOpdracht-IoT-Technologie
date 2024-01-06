833267dc73fbf06ca0b817656dbe759b6b10557e
Mauro <maurodebruyn@hotmail.com>
2024-01-06 15:50:51 +0100

    Add data from pynq

--- HTTPS_CRUD.py
+++ HTTPS_CRUD.py
@@ -1,22 +1,39 @@
-# The CRUD-endpoint URL
+#The CRUD-endpoint URL
 crud_endpoint_url = "iot.pxl.bjth.xyz/api/v1/temperature"
 
-# ??
+#Format to send
 payload = {
 
  "id": <UNIX TIME>,
 
- "value": <FLOAT>,
+ "value": TEMP,
 
- "scale": "<STRING>"
-
+ "scale": "Celsius"
 }
 
-# Headers
+#Headers
 headers = {
     "Content-Type": "application/json",
 }
 
+# Read data from files
+with open('in_temp0_raw', 'r') as f:
+    TEMP_RAW = int(f.read())
+
+with open('in_temp0_scale', 'r') as f:
+    TEMP_SCALE = float(f.read())
+
+with open('in_temp0_offset', 'r') as f:
+    TEMP_OFFSET = float(f.read())
+
+#Calculate temperature
+TEMP = ((TEMP_RAW - TEMP_OFSET) / TEMP_SCALE)
+
+#Send POST request
+requests.post(crud_endpoint_url, json=payload, headers=headers)
+
+'''
+#Debugging code
 try:
     # Stuur een POST-verzoek naar het CRUD-eindpunt
     response = requests.post(crud_endpoint_url, json=payload, headers=headers)
@@ -29,3 +46,4 @@ try:
 
 except Exception as e:
     print(f"Fout bij het uitvoeren van het verzoek: {str(e)}")
+'''
